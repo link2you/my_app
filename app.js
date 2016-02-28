5eee1df9455da36807f3777a40522c93877f357d
sangwoon <link2you@daum.net>
2016-02-28 13:29:37 +0900

    dynamic test

--- app.js
+++ app.js
@@ -2,11 +2,32 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-app.use(express.static(path.join(__dirname, '/public')));
+app.set("view engine", 'ejs');
+
+var data = {count:0};
+app.get('/', function(req, res) {
+  data.count++;
+  res.render('my_first_ejs', data);
+});
+app.get('/reset', function(req, res) {
+  data.count = 0;
+  res.render('my_first_ejs', data);
+});
+app.get('/set/count', function(req, res) {
+  if(req.query.count) data.count = req.query.count;
+  res.render('my_first_ejs', data);
+});
+app.get('/set/:num', function(req, res) {
+  data.count = req.params.num;
+  res.render('my_first_ejs', data);
+});
+//app.use(express.static(path.join(__dirname, '/views')));
 //app.use(express.static(__dirname + '/public'));
-// console.log(__dirname);
+//console.log(path.join(__dirname, '/views'));
 
-
+// app.get('/', function (req, res) {
+//   res.render('my_first_ejs');
+// });
 // app.get('/',function (req, res) {
 //   res.send('Hello World!');
 // });
